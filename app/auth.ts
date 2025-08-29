d91d5b763c7456fc7377aebb9714dcf712171321
Darklau <49363173+Darklau@users.noreply.github.com>
2025-08-29 14:34:25 +0300

    feat: remove auth and register

--- app/auth.ts
+++ app/auth.ts
@@ -4,19 +4,9 @@ import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/db";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_SECRET_ID!,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_SECRET_ID!,
-    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
@@ -28,27 +18,11 @@ export const authOptions: NextAuthOptions = {
           throw new Error("Email и пароль обязательны");
         }
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-
-        if (!user || !user.passwordHash) {
-          throw new Error("Неверные учетные данные");
-        }
-
-        const isValid = await bcrypt.compare(
-          credentials.password,
-          user.passwordHash
-        );
-        if (!isValid) {
-          throw new Error("Неверные учетные данные");
-        }
+        console.log(credentials);
 
         return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          image: user.image,
+          credentials,
+          id: "hello",
         };
       },
     }),
